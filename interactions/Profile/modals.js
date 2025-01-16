bc1391d2b49d4a717da8763705f0ef3705b52736
blgperfect <135395604+blgperfect@users.noreply.github.com>
2025-01-16 09:58:34 -0500

    ajout

--- interactions/Profile/modals.js
+++ interactions/Profile/modals.js
@@ -1,48 +1,40 @@
-const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, ModalSubmitInteraction} = require('discord.js');
+const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 
-/**
- * @param {Client} client
- * @param {ModalSubmitInteraction} interaction
- */
+module.exports = async () => {
+  const modal = new ModalBuilder()
+    .setCustomId('create-profile-modal')
+    .setTitle('Créer un profil');
 
+  modal.addComponents(
+    new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('profile-name')
+        .setLabel('Quel est votre nom ?')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true)
+    ),
+    new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('profile-location')
+        .setLabel('D’où venez-vous ?')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true)
+    ),
+    new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('profile-status')
+        .setLabel('Quel est votre statut ?')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true)
+    ),
+    new ActionRowBuilder().addComponents(
+      new TextInputBuilder()
+        .setCustomId('profile-about')
+        .setLabel('Parlez-nous de vous.')
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(false)
+    )
+  );
 
-module.exports = {
-  createProfileModal: () => {
-    const modal = new ModalBuilder()
-      .setCustomId('create-profile-modal')
-      .setTitle('Créer un profil');
-
-    modal.addComponents(
-      new ActionRowBuilder().addComponents(
-        new TextInputBuilder()
-          .setCustomId('profile-name')
-          .setLabel('Quel est votre nom ?')
-          .setStyle(TextInputStyle.Short)
-          .setRequired(true)
-      ),
-      new ActionRowBuilder().addComponents(
-        new TextInputBuilder()
-          .setCustomId('profile-location')
-          .setLabel('D’où venez-vous ?')
-          .setStyle(TextInputStyle.Short)
-          .setRequired(true)
-      ),
-      new ActionRowBuilder().addComponents(
-        new TextInputBuilder()
-          .setCustomId('profile-status')
-          .setLabel('Quel est votre statut ?')
-          .setStyle(TextInputStyle.Short)
-          .setRequired(true)
-      ),
-      new ActionRowBuilder().addComponents(
-        new TextInputBuilder()
-          .setCustomId('profile-about')
-          .setLabel('Parlez-nous de vous.')
-          .setStyle(TextInputStyle.Paragraph)
-          .setRequired(false)
-      )
-    );
-
-    return modal;
-  },
+  return modal;
 };
